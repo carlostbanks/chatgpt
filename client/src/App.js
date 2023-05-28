bfc2771b3f5f23bc939568a08940fadb39ef1200
Carlos Banks <cbanks@clypr.co>
2023-05-28 11:37:57 -0400

    sidebar in client styling done

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+const  App = () => {
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>Blu</li>
+        </ul>
+        <nav>
+          <p>Made by Carlos</p>
+        </nav>
+      </section>
+      
+      <section className="main"></section>
+
+
+    </div>
+  );
+}
+
+export default App;

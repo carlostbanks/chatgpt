ecf41bb3ee0c84fba34d84e837d8ec6dc2984857
Carlos Banks <cbanks@clypr.co>
2023-05-28 13:37:48 -0400

    content and roles from API rendering on screen

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,28 @@ import {useState, useEffect } from 'react'
 
 const App = () => {
 
+  const [value, setValue] = useState(null)
   const [message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
 
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue("")
+  }
 
   const getMessages = async () => {
     const options = {
       method: 'POST',
       body: JSON.stringify({
-        message: "Hello, how are you?"
+        message: value
       }),
       headers: {
         "Content-Type": "application/json"
@@ -19,20 +33,46 @@ const App = () => {
     try {
       const response = await fetch('http://localhost:8000/completions', options);
       const data = await response.json();
-      console.log(data);
       setMessage(data.choices[0].message)
     } catch (error) {
       console.error(error);
     }
   };
 
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if(!currentTitle && value && message ) {
+      setCurrentTitle(value)
+    } 
+    if (currentTitle && value && message ) {
+      setPreviousChats(prevChats => (
+        [...prevChats, 
+          {
+            title: currentTitle,
+            role: "user",
+            content: value
+        }, 
+        {
+            title: currentTitle,
+            role: message.role,
+            content: message.content
+
+        }]
+      ))
+    }
+  }, [message, currentTitle])
+
+  console.log("prev chats", previousChats)
+
+  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
 
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New Chat</button>
+        <button onClick={createNewChat}>+ New Chat</button>
         <ul className="history">
-          <li>Blu</li>
+          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li> )}
         </ul>
         <nav>
           <p>Made by Carlos</p>
@@ -40,13 +80,22 @@ const App = () => {
       </section>
       
       <section className="main">
-        <h1>CarlosGPT</h1>
+        {!currentTitle && <h1>CarlosGPT</h1>}
         <ul className="feed">
+          {currentChat?.map((chatMessage, index) => <li key={index}>
+            <p className='role'>
+              {chatMessage.role}
+            </p>
+            <p>
+              {chatMessage.content}
+            </p>
+          </li>)}
+
    
         </ul>
         <div className="bottom-section">
             <div className="input-container">
-              <input/>
+              <input value={value} onChange={(e) => setValue(e.target.value)}/>
               <div id="submit" onClick={getMessages}>➢</div>
             </div>
             <p className="info">
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use(cors())
 
 
-const API_KEY = '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'
+const API_KEY = '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'
 
 app.post('/completions', async (req, res) => {
 
@@ -18,7 +18,7 @@ app.post('/completions', async (req, res) => {
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: "How are you?"}],
+      messages: [{role: "user", content: req.body.message}],
       max_tokens: 100,
     })
   }
